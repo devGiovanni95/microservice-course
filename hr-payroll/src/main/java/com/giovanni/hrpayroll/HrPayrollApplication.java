cebebf98a229fb462d997dee7beef3a164ba4325
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-02-17 23:46:05 -0300

    Working with Feign client, for Requests hppt of other service

--- hr-payroll/src/main/java/com/giovanni/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/giovanni/hrpayroll/HrPayrollApplication.java
@@ -2,7 +2,10 @@ package com.giovanni.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+//quando imprementarmos a chamada, ficara muito mais limpa do que usando o restTemplete alem de estar ligado com todas as ferramentas do SprindCloud
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
--- hr-payroll/src/main/java/com/giovanni/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/giovanni/hrpayroll/services/PaymentService.java
@@ -2,13 +2,9 @@ package com.giovanni.hrpayroll.services;
 
 import com.giovanni.hrpayroll.entities.Payment;
 import com.giovanni.hrpayroll.entities.Worker;
+import com.giovanni.hrpayroll.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
@@ -18,21 +14,26 @@ public class PaymentService {
      do projeto com um nome e sua respectiva URL depois disso só fazer a chamada
       da variavel utilizando o VALUE com o nome da variavel entre cifrão e chaves
     */
-
-    @Value("${hr-worker.host}")
-    private  String workerHost;
+    //Nao iremos mais precisar usar estes dados
+//    @Value("${hr-worker.host}")
+//    private  String workerHost;
+//
+//    @Autowired
+//    private RestTemplate restTemplate;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     //Metodo com uma regra de negorio
     public Payment getPayment(long workerId, int days) {
 
-        Map<String, String>uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+ workerId);//modo de tranformar o atributo em uma string
+        //Não necessitamos mais mapear
+//        Map<String, String>uriVariables = new HashMap<>();
+//        uriVariables.put("id", ""+ workerId);//modo de tranformar o atributo em uma string
 
 //        Worker worker = restTemplate.getForObject(url, responseType)
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);//para usar o ForObject e necessario criar um mapeamento de antemao
+//        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);//para usar o ForObject e necessario criar um mapeamento de antemao
+        Worker worker = workerFeignClient.findById(workerId).getBody();
 
 //        return new Payment("Giovanni", 200.0, days);   dados mocados
         return new Payment(worker.getName(),worker.getDailyIncome(), days);
