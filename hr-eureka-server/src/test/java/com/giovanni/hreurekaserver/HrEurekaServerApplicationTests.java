bdc3eae8d10e3d655b349b703421f02abffae687
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-02-19 16:54:35 -0300

    Creating project with eureka server

---
+++ hr-eureka-server/src/test/java/com/giovanni/hreurekaserver/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovanni.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
