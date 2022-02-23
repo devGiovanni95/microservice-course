818984f384dcbdf1add82df20c2265e93a261d65
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-02-23 00:06:44 -0300

    Problems with zuul

--- hr-api-gateway-zuul/src/main/java/com/giovanni/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/giovanni/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -2,7 +2,11 @@ package com.giovanni.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient
+@EnableZuulProxy//Alem de ser uma api gateway ele é tambem  um cliente eureka server
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
