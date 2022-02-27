07579284954e73388be7384bd1e87474254c1ce5
devGiovanni95 <giovanni.devjava@hotmail.com>
2022-02-27 16:57:10 -0300

    Hr-Worker with client of server settings

--- hr-worker/src/main/java/com/giovanni/hrworker/resource/WorkerResource.java
+++ hr-worker/src/main/java/com/giovanni/hrworker/resource/WorkerResource.java
@@ -5,6 +5,7 @@ import com.giovanni.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,12 +23,23 @@ public class WorkerResource {
     //Para escrever coisas no logger
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
+    //paratestar  buscar e acessar uma variavel de configuracao la no git
+    @Value("${test.config}")
+       private String testConfig;
+
+
     //ter acesso as portas que estao sendo utilizadas
     @Autowired
     private Environment env;
 
     @Autowired
     private WorkerRepository repository;
+
+    @GetMapping(value = "/configs")
+    public ResponseEntity<Void> getConfigs() {
+        logger.info("CONFIG = " + testConfig);
+        return ResponseEntity.noContent().build();//pra nao ter corpo na resposta
+    }
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
