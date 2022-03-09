a63e192d949c74437fd8a5b8549ea3b05a5d96d0
Giovanni Almeida <giovanni.devjava@hotmail.com>
2022-03-08 23:52:15 -0300

    Creating @Bean to encrypt password for use in database

--- hr-user/src/main/java/com/giovanni/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/giovanni/hruser/HrUserApplication.java
@@ -1,15 +1,27 @@
 package com.giovanni.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
+
+	//pra criptografar senhas mas necessitamos instanciar andes de usar pois ele nao vem instanciado mas vem junto com os pacotes do springboot
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder ;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println("BCRYPT = " + passwordEncoder.encode("12345"));
+
+	}
 }
