51e2a7d6ab944bfffce5b6b19e769cec72cf3d6d
Giovanni Almeida <giovanni.devjava@hotmail.com>
2022-03-19 18:30:12 -0300

    Cors Configuration

--- hr-api-gateway-zuul/src/main/java/com/giovanni/hrapigatewayzuul/config/AppConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/giovanni/hrapigatewayzuul/config/AppConfig.java
@@ -4,15 +4,18 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @RefreshScope
+@EnableAuthorizationServer
 @Configuration
 public class AppConfig {
 
     @Value("${jwt.secret}")
     private String jwtSecret;
+
     @Bean
     public JwtAccessTokenConverter accessTokenConverter(){
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
